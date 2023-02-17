eaca27403b04a80f17af3beb87f37737a11c1c0e
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-17 14:48:42 +0200

    add loader and image to preview profile

--- components/Profile/PreviewProfile.js
+++ components/Profile/PreviewProfile.js
@@ -1,26 +1,49 @@
-import { useContext, useEffect, useState } from 'react'
-import { View, Text, Button } from 'react-native'
-import { AuthContext } from '../../store/auth-context'
-import { getProfileData } from '../../util/profile'
+import { useContext, useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { AuthContext } from "../../store/auth-context";
+import { getProfileData } from "../../util/profile";
+import LoadingOverlay from "../UI/LoadingOverlay";
 function PreviewProfile() {
-    const authCtx = useContext(AuthContext)
-    const [profileData, setProfileData] = useState()
+    const authCtx = useContext(AuthContext);
+    const [profileData, setProfileData] = useState();
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         async function getProfile() {
             try {
-                const response = await getProfileData(authCtx.idToken)
+                setIsLoading(true)
+                const response = await getProfileData(authCtx.idToken);
                 // console.log(response);
-                setProfileData(response)
+                setProfileData(response.data.data);
             } catch (error) {
                 console.log(error);
             }
+            setIsLoading(false)
         }
-        getProfile()
-    }, [])
+        getProfile();
+    }, []);
     console.log(profileData);
-    return <View><Text>Logged in</Text>
-        <Button title='logout' onPress={authCtx.removeIdToken} />
-    </View>
+    if (isLoading) {
+        return <LoadingOverlay />
+    }
+    return (
+        <View>
+            <View>
+                <Image source={{ uri: profileData?.image }} style={{ width: 200, height: 200 }} />
+            </View>
+            <View>
+                <Text></Text>
+            </View>
+            <View>
+                <Text></Text>
+            </View>
+            <View>
+                <Text></Text>
+            </View>
+            <View>
+                <Text></Text>
+            </View>
+        </View>
+    );
 }
 
-export default PreviewProfile
+export default PreviewProfile;
---
+++ components/UI/LoadingOverlay.js
@@ -0,0 +1,19 @@
+import { View, ActivityIndicator, StyleSheet } from "react-native";
+
+const LoadingOverlay = () => {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color="black" />
+        </View>
+    );
+};
+
+export default LoadingOverlay;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 24,
+    },
+})
