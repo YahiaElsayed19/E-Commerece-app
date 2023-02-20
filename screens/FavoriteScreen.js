f6d07b574cd641fcf3d6c6ad5e6ba7d6bdd65b53
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-20 14:05:20 +0200

    getData to fav screen

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native"
-
+import { useContext } from "react";
+import { View, FlatList } from "react-native";
+import { useQuery } from "react-query";
+import Product from "../components/products/Product";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
+import { getFav } from "../util/products";
 function FavoriteScreen() {
-  return <View>
-  <Text>Favorite Screen</Text>
-</View>
+  const authCtx = useContext(AuthContext)
+  const { data, isLoading } = useQuery("GetFavProducts", () => getFav(authCtx.idToken));
+  // console.log(data.data.data.data[0]);
+  let products = []
+  for (let i = 0; i < data?.data.data.data.length; i++) {
+    products.push(data?.data.data.data[i].product)
+  }
+  console.log(products);
+  if (isLoading) {
+    return <LoadingOverlay />
+  }
+  function renderProduct(itemData) {
+    return <Product product={itemData.item} />
+  }
+  return (
+    <View>
+      <FlatList data={products} numColumns={2} keyExtractor={(item) => item.id} renderItem={renderProduct} />
+    </View>
+  );
 }
+
 
 export default FavoriteScreen
