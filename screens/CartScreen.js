d3eaf89ecd5495bc59ffc4c03d81bee52917130a
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-20 17:29:21 +0200

    add total cost to cart screen

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,11 +1,12 @@
 import { useContext, useEffect } from "react";
-import { View, FlatList } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 import { useQuery } from "react-query";
 import Product from "../components/products/Product";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { AuthContext } from "../store/auth-context";
 import { getCart } from "../util/products";
 import { useIsFocused } from "@react-navigation/native";
+import Colors from "../constants/Colors";
 
 function CartScreen() {
     const isFocused = useIsFocused();
@@ -14,6 +15,7 @@ function CartScreen() {
         "GetCartProducts",
         () => getCart(authCtx.idToken)
     );
+    // console.log(data.data);
     // console.log(data.data.data["cart_items"]);
     // console.log(data?.data.data["cart_items"][0].product);
     useEffect(() => {
@@ -26,26 +28,61 @@ function CartScreen() {
         products.push(data.data.data["cart_items"][i].product);
     }
 
-    // console.log(products);  
+    // console.log(products);
     if (isLoading) {
         return <LoadingOverlay />;
     }
     if (isRefetching) {
         return <LoadingOverlay />;
     }
+    if (products.length === 0) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text
+                    style={{ color: Colors.primary100, fontWeight: "bold", fontSize: 18 }}
+                >
+                    There is nothing in your cart yet!
+                </Text>
+            </View>
+        );
+    }
     function renderProduct(itemData) {
         return <Product product={itemData.item} />;
     }
     return (
-        <View>
-            <FlatList
-                data={products}
-                numColumns={2}
-                keyExtractor={(item) => item.id}
-                renderItem={renderProduct}
-            />
+        <View style={{ flex: 1 }}>
+            <View style={styles.totalContainer}>
+                <Text style={styles.title}>Total</Text>
+                <Text style={styles.total}>{data.data.data.total} L.E.</Text>
+            </View>
+            <View style={{ flex: 10 }}>
+                <FlatList
+                    data={products}
+                    numColumns={2}
+                    keyExtractor={(item) => item.id}
+                    renderItem={renderProduct}
+                />
+            </View>
         </View>
     );
 }
 
 export default CartScreen;
+const styles = StyleSheet.create({
+    totalContainer: {
+        backgroundColor:"white",
+        padding:12,
+        margin:12,
+        borderRadius:6,
+        flexDirection:"row",
+        justifyContent:"space-between",
+    },
+    title: {
+        color:Colors.primary100,
+        fontWeight:"bold",
+        fontSize:18,
+    },
+    total: {
+        fontSize:18,
+    },
+});
--- screens/FavouriteScreen.js
+++ screens/FavouriteScreen.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import { View, FlatList } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { useQuery } from "react-query";
 import Product from "../components/products/Product";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
@@ -7,34 +7,54 @@ import { AuthContext } from "../store/auth-context";
 import { getProducts } from "../util/products";
 import { useIsFocused } from "@react-navigation/native";
 function FavouriteScreen() {
-    const isFocused = useIsFocused()
-    const authCtx = useContext(AuthContext)
-    const { data, isLoading, refetch, isRefetching } = useQuery("GetFavouriteProducts", () => getProducts(authCtx.idToken));
+    const isFocused = useIsFocused();
+    const authCtx = useContext(AuthContext);
+    const { data, isLoading, refetch, isRefetching } = useQuery(
+        "GetFavouriteProducts",
+        () => getProducts(authCtx.idToken)
+    );
     // console.log(data.data.data.data[0]);
     useEffect(() => {
         if (isFocused) {
-            refetch()
+            refetch();
         }
-    }, [isFocused])
+    }, [isFocused]);
     const products = data?.data.data.data;
     // console.log(products);
-    const filterdProducts = products?.filter((product) => product['in_favorites'] === true)
+    const filterdProducts = products?.filter(
+        (product) => product["in_favorites"] === true
+    );
     // console.log(products);
     if (isLoading) {
-        return <LoadingOverlay />
+        return <LoadingOverlay />;
     }
     if (isRefetching) {
-        return <LoadingOverlay />
+        return <LoadingOverlay />;
+    }
+    if (filterdProducts.length === 0) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text
+                    style={{ color: Colors.primary100, fontWeight: "bold", fontSize: 18 }}
+                >
+                    There is nothing in your favourite yet!
+                </Text>
+            </View>
+        );
     }
     function renderProduct(itemData) {
-        return <Product product={itemData.item} />
+        return <Product product={itemData.item} />;
     }
     return (
         <View>
-            <FlatList data={filterdProducts} numColumns={2} keyExtractor={(item) => item.id} renderItem={renderProduct} />
+            <FlatList
+                data={filterdProducts}
+                numColumns={2}
+                keyExtractor={(item) => item.id}
+                renderItem={renderProduct}
+            />
         </View>
     );
-
 }
 
-export default FavouriteScreen
+export default FavouriteScreen;
