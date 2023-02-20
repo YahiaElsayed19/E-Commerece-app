96b2cacafe48d6083bc2ce6d6e1c2d36151368a4
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-20 16:26:20 +0200

    CartScreen done

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,9 +1,51 @@
-import { View, Text } from "react-native"
+import { useContext, useEffect } from "react";
+import { View, FlatList } from "react-native";
+import { useQuery } from "react-query";
+import Product from "../components/products/Product";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
+import { getCart } from "../util/products";
+import { useIsFocused } from "@react-navigation/native";
 
 function CartScreen() {
-    return <View>
-    <Text>Cart Screen</Text>
-</View>
+    const isFocused = useIsFocused();
+    const authCtx = useContext(AuthContext);
+    const { data, isLoading, refetch, isRefetching, isFetchedAfterMount } = useQuery(
+        "GetProducts",
+        () => getCart(authCtx.idToken)
+    );
+    // console.log(data.data.data["cart_items"]);
+    // console.log(data?.data.data["cart_items"][0].product);
+    useEffect(() => {
+        if (isFocused) {
+            refetch();
+        }
+    }, [isFocused]);
+    let products = [];
+    for (let i = 0; i < data?.data?.data["cart_items"]?.length; i++) {
+        products.push(data.data.data["cart_items"][i].product);
+    }
+
+    console.log(products);  
+    if (isLoading) {
+        return <LoadingOverlay />;
+    }
+    if (isRefetching) {
+        return <LoadingOverlay />;
+    }
+    function renderProduct(itemData) {
+        return <Product product={itemData.item} />;
+    }
+    return (
+        <View>
+            <FlatList
+                data={products}
+                numColumns={2}
+                keyExtractor={(item) => item.id}
+                renderItem={renderProduct}
+            />
+        </View>
+    );
 }
 
-export default CartScreen
+export default CartScreen;
