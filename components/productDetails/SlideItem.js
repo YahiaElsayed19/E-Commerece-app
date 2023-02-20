6a644e08760214355a786ecd35529faf4bdda822
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-20 19:49:58 +0200

    add image sldier to product details

---
+++ components/productDetails/SlideItem.js
@@ -0,0 +1,37 @@
+import {
+    Image,
+    StyleSheet,
+    View,
+    Dimensions,
+} from 'react-native';
+import React from 'react';
+
+const { width } = Dimensions.get('screen');
+
+const SlideItem = ({ item }) => {
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{ uri: item }}
+                resizeMode="cover"
+                style={[
+                    styles.image
+                ]}
+            />
+        </View>
+    );
+};
+
+export default SlideItem;
+
+const styles = StyleSheet.create({
+    container: {
+        width,
+        height:300,
+    },
+    image: {
+        height:350,
+        width: '100%',
+    },
+});
--- screens/ProductDetailsScreen.js
+++ screens/ProductDetailsScreen.js
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native"
+import Slider from "../components/productDetails/Slider";
 
-function ProductDetailsScreen({route}) {
-    console.log(route.params.product.name);
+function ProductDetailsScreen({ route }) {
+    // console.log(route.params.product.name);
     return <View>
-    <Text> Screen</Text>
-</View>
+        <Slider slides={route.params.product.images}/>
+    </View>
 }
 
 export default ProductDetailsScreen
