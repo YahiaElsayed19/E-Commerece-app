3af0d919e3b8f1017fb7f61c697805108357ca12
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-20 14:46:09 +0200

    refetch data when screen focused

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,14 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { View, FlatList } from "react-native";
 import { useQuery } from "react-query";
 import Product from "../components/products/Product";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { AuthContext } from "../store/auth-context";
 import { getFav } from "../util/products";
+import { useIsFocused } from "@react-navigation/native";
 function FavoriteScreen() {
+  const isFocused = useIsFocused()
   const authCtx = useContext(AuthContext)
-  const { data, isLoading } = useQuery("GetFavProducts", () => getFav(authCtx.idToken));
+  const { data, isLoading, refetch } = useQuery("GetFavProducts", () => getFav(authCtx.idToken));
   // console.log(data.data.data.data[0]);
+  useEffect(() => {
+    if (isFocused) {
+      refetch()
+    }
+  }, [isFocused])
   let products = []
   for (let i = 0; i < data?.data.data.data.length; i++) {
     products.push(data?.data.data.data[i].product)
@@ -18,7 +25,7 @@ function FavoriteScreen() {
     return <LoadingOverlay />
   }
   function renderProduct(itemData) {
-    return <Product product={itemData.item} inFav={true}/>
+    return <Product product={itemData.item} inFav={true} />
   }
   return (
     <View>
