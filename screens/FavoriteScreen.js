51441aef51d60ae0c6d6fe7affe3f4a30a464824
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-20 15:27:59 +0200

    handle refetch

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -7,35 +7,39 @@ import { AuthContext } from "../store/auth-context";
 import { getFav } from "../util/products";
 import { useIsFocused } from "@react-navigation/native";
 function FavoriteScreen() {
-  const isFocused = useIsFocused()
-  const authCtx = useContext(AuthContext)
-  const { data, isLoading, refetch ,isRefetching} = useQuery("GetFavProducts", () => getFav(authCtx.idToken));
+  const isFocused = useIsFocused();
+  const authCtx = useContext(AuthContext);
+  const { data, isLoading, refetch } = useQuery("GetFavProducts", () =>
+    getFav(authCtx.idToken)
+  );
   // console.log(data.data.data.data[0]);
   useEffect(() => {
     if (isFocused) {
-      refetch()
+      refetch();
     }
-  }, [isFocused])
-  let products = []
+  }, [isFocused]);
+  let products = [];
   for (let i = 0; i < data?.data.data.data.length; i++) {
-    products.push(data?.data.data.data[i].product)
+    products.push(data?.data.data.data[i].product);
   }
   // console.log(products);
   if (isLoading) {
-    return <LoadingOverlay />
+    return <LoadingOverlay />;
   }
-  if (isRefetching) {
-    return <LoadingOverlay />
-  }
+
   function renderProduct(itemData) {
-    return <Product product={itemData.item} inFav={true} />
+    return <Product product={itemData.item} inFav={true} />;
   }
   return (
     <View>
-      <FlatList data={products} numColumns={2} keyExtractor={(item) => item.id} renderItem={renderProduct} />
+      <FlatList
+        data={products}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        renderItem={renderProduct}
+      />
     </View>
   );
 }
 
-
-export default FavoriteScreen
+export default FavoriteScreen;
