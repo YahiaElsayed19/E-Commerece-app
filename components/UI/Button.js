3b0803b57e01212423f144b3a4c39ec8fa45ef6d
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-23 19:49:06 +0200

    Button take styles from outside PaymentModal content

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,7 +1,7 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 import Colors from "../../constants/Colors";
-function Button({ title, onPress }) {
-    return <Pressable style={({ pressed }) => [styles.buttonContainer, pressed && styles.pressed]} onPress={onPress}>
+function Button({ title, onPress, style }) {
+    return <Pressable style={({ pressed }) => [styles.buttonContainer, style, pressed && styles.pressed]} onPress={onPress}>
         <View style={styles.button}>
             <Text style={styles.title}>{title}</Text>
         </View>
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
         overflow: "hidden",
         width: "100%",
         marginTop: 24,
-        elevation:4,
+        elevation: 4,
     },
     button: {
         backgroundColor: Colors.primary100,
---
+++ components/cart/PaymentModal.js
@@ -0,0 +1,9 @@
+import {View,Text} from 'react-native'
+
+function PaymentModal() {
+    return <View>
+        <Text>PaymentModal</Text>
+    </View>
+}
+
+export default PaymentModal
