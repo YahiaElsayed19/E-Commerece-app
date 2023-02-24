2aa96057c1fb51e847683b3cc321053830d9caca
Yahia Elsayed <yahiaelsayed19@gmail.com>
2023-02-24 17:38:09 +0200

    handle changing request

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,16 +13,17 @@ function HomeScreen({ navigation }) {
   const isFocused = useIsFocused();
   const authCtx = useContext(AuthContext);
   const catCtx = useContext(CatContext);
-  const { data, isLoading, refetch, isRefetching } = useQuery(
+  const { data, isLoading, refetch, isRefetching, remove } = useQuery(
     "GetProducts",
-    () => getProducts(authCtx.idToken,catCtx.category)
+    () => getProducts(authCtx.idToken, catCtx.category)
   );
   // console.log(data.data.data.data[0]);
   useEffect(() => {
     if (isFocused) {
+      remove()
       refetch();
     }
-  }, [isFocused,catCtx.category]);
+  }, [isFocused, catCtx.category]);
   const products = data?.data.data.data;
   // console.log(products);
   function onFocusSearch() {
